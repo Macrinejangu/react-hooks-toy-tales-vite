8c044961680975ab1eb3732a2e170fbebc73cb90
Macrine <macrinejangu@gmail.com>
2026-08-14 02:02:08 +0300

    Fetch and display all toys on page load

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,19 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import ToyForm from "./ToyForm";
 import ToyContainer from "./ToyContainer";
 
+const BASE_URL = "http://localhost:3001/toys";
+
 function App() {
   const [showForm, setShowForm] = useState(false);
+  const [toys, setToys] = useState([]);
+
+  useEffect(() => {
+    fetch(BASE_URL)
+      .then((res) => res.json())
+      .then((toysFromServer) => setToys(toysFromServer));
+  }, []);
 
   function handleClick() {
     setShowForm((showForm) => !showForm);
@@ -18,7 +26,7 @@ function App() {
       <div className="buttonContainer">
         <button onClick={handleClick}>Add a Toy</button>
       </div>
-      <ToyContainer />
+      <ToyContainer toys={toys} />
     </>
   );
 }
